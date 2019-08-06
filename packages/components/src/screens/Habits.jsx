603e17b3b7f89d016e9724611fca2c516369dda8
lauvrenn <patiw0007@gmail.com>
2019-08-07 00:38:38 +0200

    feat: new habit button

--- packages/components/src/screens/Habits.jsx
+++ packages/components/src/screens/Habits.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
+import { StyleSheet, Text, View, Image, FlatList, Button, TouchableOpacity, Icon, ScrollView } from 'react-native'
 
 function Habits () {
+
+  // var Dimensions = require('Dimensions');
+  // var {width, height} = Dimensions.get('window');
+
   const mockData = [
     { id: 0, title: 'Tree planting', emoji: '🌳' },
     { id: 1, title: 'Tree planting', emoji: '🌳' },
     { id: 2, title: 'Tree planting', emoji: '🌳' }
   ]
 
-  return (
-    <View>
-      <View style={styles.center}>
-        <Text></Text>
-        <Text style={styles.title}>Habits</Text>
-        <Image source={require('../../assets/logo.png')} style={{ height: '32px', width: '32px' }} />
-      </View>
+  const addNewHabit = () => {
+    console.log('add new habit');
+  }
 
-      <FlatList
-        data={mockData}
-        renderItem={({ item }) => (
-          <View style={styles.listItem}>
-            <Text>{item.emoji} {item.title}</Text>
-          </View>
-        )}
-      />
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <View style={styles.center}>
+          <Text></Text>
+          <Text style={styles.title}>Habits</Text>
+          <Image source={require('../../assets/logo.png')} style={{ height: '32px', width: '32px' }} />
+        </View>
+
+        <FlatList
+          data={mockData}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <Text>{item.emoji} {item.title}</Text>
+            </View>
+          )}
+        />
+      </ScrollView>
+
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={addNewHabit} style={styles.plusButton}>
+          <Text style={styles.plusButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -44,7 +60,30 @@ const styles = StyleSheet.create({
     padding: 16,
     borderColor: '#000000',
     borderBottomWidth: 1
-  }
+  },
+  plusButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+    backgroundColor: '#000',
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  plusButtonText: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  footer: {
+    position: 'fixed',
+    height: 50,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
 })
 
 export default Habits
