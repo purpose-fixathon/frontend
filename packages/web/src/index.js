a522d813a7c29f0b5c2051070ecc5b640009f7e1
Keziah <keziah.moselle@gmail.com>
2019-08-07 17:13:15 +0200

    fix: cannot find module

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,8 +1,8 @@
-import { AppRegistry } from 'react-native'
-
-import App from 'components/src/App'
+import { AppRegistry } from 'react-native';
+import App from '../../components/src/App';
 
 AppRegistry.registerComponent('purpose', () => App)
+
 AppRegistry.runApplication('purpose', {
   rootTag: document.getElementById('root'),
 })
